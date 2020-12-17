304eb127079c7eeb791c211339ecb635b2206f0d
Haris Pasha Dipto <Haris Pasha Dipto>
2020-12-17 11:55:54 +0600

    calling api endpoints from react

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <Router>
-                <Switch>
-                    <Route exact path="/"><p>This is the home page</p></Route>
-                    <Route path="/join" component={RoomJoinPage}/>
-                    <Route path="/create" component={CreateRoomPage}/>
-                </Switch>
-            </Router>
-        ) 
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
